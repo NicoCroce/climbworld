82a5ff0f08c1f4ed534bd1ec48accfba609a5640
NicoCRoce <ingcrocenicolas@outlook.com>
2015-10-29 09:02:53 -0300

    Cabecera y pie de la página terminados. Se comenzó con el carrusel.

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,12 +1,57 @@
 "use strict";
 //******************************************    seccion variables    ******************************************
-	
+	var imgIndex = 1;
 //**********************************************************************************************
 
 //******************************************    onLoad    ******************************************
 jQuery(document).ready(function($) {
 	$('#headerSection').load('header.html');
 	$('#footerSection').load('footer.html');
+	$('#contentSection').load('home.html');
+
+	$(document).on('click', '.next', function(){
+		$('#mask').removeClass().addClass('mask');
+		switch(imgIndex){
+			case 0:
+				$('#mask').addClass('img0-selected');
+				imgIndex = 1;
+				break;
+			case 1:
+				$('#mask').addClass('img1-selected');
+				imgIndex = 2
+				break;
+			case 2:
+				$('#mask').addClass('img2-selected');
+				imgIndex = 0;
+				break;
+			default:
+				statements_def
+				break;
+		}
+		
+	});
+
+	$(document).on('click', '.prev', function(){
+		$('#mask').removeClass().addClass('mask');
+		switch(imgIndex){
+			case 0:
+				$('#mask').addClass('img0-selected');
+				imgIndex = 2;
+				break;
+			case 1:
+				$('#mask').addClass('img1-selected');
+				imgIndex = 0;
+				break;
+			case 2:
+				$('#mask').addClass('img2-selected');
+				imgIndex = 1;
+				break;
+			default:
+				statements_def
+				break;
+		}
+		
+	});
 });
 
 //**********************************************************************************************
