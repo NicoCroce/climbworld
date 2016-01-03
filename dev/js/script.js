965f342b0a1e25edf3b4369f034d9627367603af
NicoCroce <ingcrocenicolas@outlook.com>
2016-01-03 19:17:53 -0300

    Corregir carrusel de imágenes.

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 //******************************************    seccion variables    ******************************************
 	var imgIndex = 1;
+	var totalImg = 3;
 	var sectionName = "";
 	var height = {
 		header: '',
@@ -39,25 +40,38 @@ jQuery(document).ready(function($) {
 	$('#productsSection').load('products.html');
 	$('#contactSection').load('contact.html');
 	$(document).on('click', '.next', function(){
-		$('#mask').removeClass().addClass('mask');
-		switch(imgIndex){
-			case 0:
-				$('#mask').addClass('img0-selected');
-				imgIndex = 1;
-				break;
-			case 1:
-				$('#mask').addClass('img1-selected');
-				imgIndex = 2
-				break;
-			case 2:
-				$('#mask').addClass('img2-selected');
-				imgIndex = 0;
-				break;
-			default:
-				statements_def
-				break;
-		}
-		
+		// if (imgIndex == totalImg) { 
+		// 	imgIndex = 1;
+		// 	$('.img'+totalImg).removeClass('fade-in-grow').removeClass('fade-out-grow').addClass('fade-out-grow');
+		// 	$('.img'+imgIndex).removeClass('fade-in-grow').removeClass('fade-out-grow').addClass('fade-in-grow');
+		// 	return;
+		// };
+		// imgIndex++;
+		$('.img'+imgIndex).removeClass('fade-in-grow').removeClass('fade-out-grow').addClass('fade-out-grow');
+		imgIndex++;
+		$('.img'+imgIndex).removeClass('fade-in-grow').removeClass('fade-out-grow').addClass('fade-in-grow');
+		if (imgIndex == totalImg) { 
+			imgIndex = 1;
+			$('.img'+imgIndex).removeClass('fade-in-grow').removeClass('fade-out-grow').addClass('fade-in-grow');
+		};
+		// $('#mask').removeClass().addClass('mask');
+		// switch(imgIndex){
+		// 	case 0:
+		// 		$('#mask').addClass('img0-selected');
+		// 		imgIndex = 1;
+		// 		break;
+		// 	case 1:
+		// 		$('#mask').addClass('img1-selected');
+		// 		imgIndex = 2
+		// 		break;
+		// 	case 2:
+		// 		$('#mask').addClass('img2-selected');
+		// 		imgIndex = 0;
+		// 		break;
+		// 	default:
+		// 		statements_def
+		// 		break;
+		// }
 	});
 
 	$(document).on('click', '.prev', function(){
