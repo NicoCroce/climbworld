cacbb5e8062732af93e920ddcf886a66593031d9
NicoCRoce <ingcrocenicolas@outlook.com>
2016-11-07 00:20:04 -0300

    Format code

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,70 +1,74 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	var offsetScroll = 100;
+    var offsetScroll = 100;
 
-	$(window).scroll(function (event) {
-		console.log('entro');
-		if (!isReady) { return; };
+    $(window).scroll(function(event) {
+        console.log('entro');
+        if (!isReady) {
+            return;
+        };
 
-		var scroll = $(window).scrollTop();
+        var scroll = $(window).scrollTop();
 
-		if (positions.navBar == 0) {
-			positions.navBar = $('.nav-bar').position().top;
-		}
+        if (positions.navBar == 0) {
+            positions.navBar = $('.nav-bar').position().top;
+        }
 
-		if (scroll > beforeScroll) {
-			$('.bg-header-mobile').addClass('hidden');
-		}else{
-			$('.bg-header-mobile').removeClass('hidden');
-		}
+        if (scroll > beforeScroll) {
+            $('.bg-header-mobile').addClass('hidden');
+        } else {
+            $('.bg-header-mobile').removeClass('hidden');
+        }
 
-		beforeScroll = scroll;
-		if(scroll >= positions.navBar){
-			$('.nav-bar').addClass('stack-nav-bar');
-		}else {
-			$('.nav-bar').removeClass('stack-nav-bar');
-		}
+        beforeScroll = scroll;
+        if (scroll >= positions.navBar) {
+            $('.nav-bar').addClass('stack-nav-bar');
+        } else {
+            $('.nav-bar').removeClass('stack-nav-bar');
+        }
 
-		if (scroll >= height.header) {
-			$('.bg-header-mobile').removeClass('hidden-bg');
-		}else{
-			$('.bg-header-mobile').addClass('hidden-bg');
-		}
-		if(isClicked){
-			return;
-		}
-		if (scroll < positions.company) {
-			changeSection('home');
-		}else if(scroll < positions.products){
-			changeSection('company');
-		}else if(scroll < positions.contact){
-			changeSection('products');
-		}else{
-			changeSection('contact');
-		}
+        if (scroll >= height.header) {
+            $('.bg-header-mobile').removeClass('hidden-bg');
+        } else {
+            $('.bg-header-mobile').addClass('hidden-bg');
+        }
+        if (isClicked) {
+            return;
+        }
+        if (scroll < positions.company) {
+            changeSection('home');
+        } else if (scroll < positions.products) {
+            changeSection('company');
+        } else if (scroll < positions.contact) {
+            changeSection('products');
+        } else {
+            changeSection('contact');
+        }
 
-		animate_elems();
-	});
+        animate_elems();
+    });
 
-	function animate_elems() {
-	    var wintop = $(window).scrollTop(), // calculate distance from top of window
-	    allElemmentsToAnimate = $('.animateblock'),
-		winheight = $(window).height(),
-		fullheight = $(document).height();
+    function animate_elems() {
+        var wintop = $(window).scrollTop(), // calculate distance from top of window
+            allElemmentsToAnimate = $('.animateblock'),
+            winheight = $(window).height(),
+            fullheight = $(document).height();
 
-	    // loop through each item to check when it animates
-	    allElemmentsToAnimate.each(function(){
-	    	var objectAnimated = $(this);
+        // loop through each item to check when it animates
+        allElemmentsToAnimate.each(function() {
+            var objectAnimated = $(this);
 
-	      if(objectAnimated.hasClass('animated')) { return true; } // if already animated skip to the next item
+            if (objectAnimated.hasClass('animated')) {
+                return true;
+            } // if already animated skip to the next item
 
-	      var topcoords = objectAnimated.offset().top; // element's distance from top of page in pixels
+            var topcoords = objectAnimated.offset().top; // element's distance from top of page in pixels
 
-	      	if(wintop > (topcoords - (winheight*.75))) {
-		        // animate when top of the window is 3/4 above the element
-		        objectAnimated.addClass('animated ' + objectAnimated.attr('fxAnimation'));
-	    	}
-		});
-	} // end animate_elems()
+            if (wintop > (topcoords - (winheight * .9))) {
+                // animate when top of the window is 3/4 above the element
+                objectAnimated.addClass('animated ' + objectAnimated.attr('fxAnimation'));
+            }
+        });
+    } // end animate_elems()
 
 });
